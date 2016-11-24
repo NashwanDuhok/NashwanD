ec14fd6a4946e03ba4363c5983df287511f62cc4
Nipeal <nipeal@Nipeals-MacBook-Pro.local>
2016-11-24 18:01:53 +0300

    Initial Commit

---
+++ nas/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  nas
+//
+//  Created by C.Ronaldo on 11/24/16.
+//  Copyright © 2016 C.Ronaldo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ nas/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  nas
+//
+//  Created by C.Ronaldo on 11/24/16.
+//  Copyright © 2016 C.Ronaldo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
